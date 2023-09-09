86568d896a9ccc0d8a00a837e810c743f60a3382
Reuben Raj <reuben.raj@thredd.com>
2023-09-09 22:26:11 +0100

    Added bulma

--- src/App.js
+++ src/App.js
@@ -5,11 +5,36 @@ import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
+    <>
     <div>
-      heelo
-      <LoginButton />
-      <LogoutButton />
+      <nav class="navbar is-white has-shadow">
+        <div class="navbar-brand">
+          <a class="navbar-item"><img src="assets/logo.png" /></a>
+        </div>
+        <div class="navbar-menu" id="nav-links">
+          <div class="navbar-end">
+            <LoginButton />
+            <LogoutButton />
+         </div>
+      </div>
+      </nav>
+      {/* <nav class="navbar is-white has-shadow">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="assets/logo.png" style="max-height: 70px" class="py-2 px-2" />
+      </a>
     </div>
+  
+    <div class="navbar-menu" id="nav-links">
+      <div class="navbar-end">
+        <a class="navbar-item">My Account</a>
+        <a class="navbar-item">Shopping Cart (0)</a>
+      </div>
+    </div>
+  </nav> */}
+      
+    </div>
+      </>
 
   );
 }
